55dd284841c6451f57181c8479001aa992e75a30
JoshChelen <jchelen@udel.edu>
2025-05-03 22:09:22 -0400

    side panel now opens on the active tab

--- public/background.js
+++ public/background.js
@@ -1,13 +1,8 @@
 chrome.runtime.onMessage.addListener((msg, sender) => {
   if (msg === "open-side-panel") {
-    const windowId = sender.tab?.windowId;
-
-    if (windowId !== undefined) {
-      chrome.sidePanel.open({ windowId });
-    } else {
-      chrome.windows.getCurrent({}, (win) => {
-        chrome.sidePanel.open({ windowId: win.id });
-      });
-    }
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const tab = tabs[0];
+        chrome.sidePanel.open({ windowId: tab.windowId });
+    });
   }
 });
